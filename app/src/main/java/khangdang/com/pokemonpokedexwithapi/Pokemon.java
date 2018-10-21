a0dcbdcefc1da3efcee72494d35090e8aaf646e0
khangdang <khangdang@mail.adephi.edu>
2018-10-21 16:02:17 -0400

    First attempt at getting info using Retrofit

--- app/src/main/java/khangdang/com/pokemonpokedexwithapi/Pokemon.java
+++ app/src/main/java/khangdang/com/pokemonpokedexwithapi/Pokemon.java
@@ -30,14 +30,7 @@ public class Pokemon {
         setPokemonName(name);
         setHeight(height);
         setWeight(weight);
-        /*
-        setCategory(category);
-        setHP(HP);
-        setAttack(att);
-        setDefense(def);
-        setSpAttack(spatt);
-        setSpDefense(spdef);
-        setSpeed(speed);*/
+      
     }
 
 
@@ -76,60 +69,6 @@ public class Pokemon {
     }
 
 
-        public String getCategory() {
-            return mCategory;
-        }
 
-        public void setCategory(String mCategory) {
-            this.mCategory = mCategory;
-        }
-
-        public int getHP() {
-            return mHP;
-        }
-
-        public void setHP(int mHP) {
-            this.mHP = mHP;
-        }
-
-        public int getAttack() {
-            return mAttack;
-        }
-
-        public void setAttack(int mAttack) {
-            this.mAttack = mAttack;
-        }
-
-        public int getDefense() {
-            return mDefense;
-        }
-
-        public void setDefense(int mDefense) {
-            this.mDefense = mDefense;
-        }
-
-        public int getSpAttack() {
-            return mSpAttack;
-        }
-
-        public void setSpAttack(int mSpAttack) {
-            this.mSpAttack = mSpAttack;
-        }
-
-        public int getSpDefense() {
-            return mSpDefense;
-        }
-
-        public void setSpDefense(int mSpDefense) {
-            this.mSpDefense = mSpDefense;
-        }
-
-        public int getSpeed() {
-            return mSpeed;
-        }
-
-        public void setSpeed(int mSpeed) {
-            this.mSpeed = mSpeed;
-        }*/
-
+*/
 }
